bde7e1b46d2bcde968580c96b46ccedfece9d9e1
Germano Ciarlini <germano@ar.quei.ro>
2021-05-17 21:00:56 -0300

    Modularize load modal

---
+++ src/components/modal/LoadModal.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./LoadModal.css";
+
+const LoadModal = (props) => {
+  const { collections, onLoad } = props;
+  const [selectedCollection, setSelectedCollection] = useState(null);
+
+  const onSelectedCollectionHandler = (collection) => {
+    setSelectedCollection(collection);
+  };
+
+  const onLoadHandler = () => {
+    onLoad(selectedCollection);
+  };
+
+  return (
+    <div>
+      <header className="modal__header">
+        <h2>Load Feature Collection</h2>
+        <button
+          className="modal__select-button"
+          onClick={onLoadHandler}
+          disabled={selectedCollection === null}
+        >
+          Load Collection
+        </button>
+      </header>
+      <main>
+        {collections.map((collection) => {
+          return (
+            <div
+              className={`modal__collection-item ${
+                collection === selectedCollection ? "selected" : ""
+              }`}
+              onClick={() => onSelectedCollectionHandler(collection)}
+              key={collection.id}
+            >
+              <span>{collection.name}</span>
+              <span>{collection.updatedAt.toLocaleString("default")}</span>
+            </div>
+          );
+        })}
+      </main>
+    </div>
+  );
+};
+
+export default LoadModal;
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,36 +1,29 @@
 import { faFolderOpen, faSave } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import Modal from "react-modal";
+import api from "../../services/api";
+import LoadModal from "../modal/LoadModal";
 import "./Toolbar.css";
 import ToolbarButton from "./ToolbarButton";
-import api from "../../services/api";
-import Modal from "react-modal";
-import { useState } from "react";
 
 Modal.setAppElement(document.getElementById("root"));
 
 const Toolbar = (props) => {
   const { onSave, onLoad } = props;
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeModal, setActiveModal] = useState(null);
   const [collections, setCollections] = useState([]);
-  const [selectedCollection, setSelectedCollection] = useState(null);
 
-  //#region Save Handlers
   const onSaveHandler = () => {
+    onSetActiveModalHandler("save-modal");
     onSave();
   };
-  //#endregion
 
-  //#region Load Handlers
-  const _onModalOpen = () => {
-    setIsModalOpen(true);
-  };
-
-  const _onModalClose = () => {
-    setIsModalOpen(false);
-    setSelectedCollection(null);
+  const onSetActiveModalHandler = (modal) => {
+    setActiveModal(modal);
   };
 
   const onLoadClickHandler = async () => {
-    _onModalOpen();
+    onSetActiveModalHandler("load-modal");
     const featureCollections = await api.get("/collections");
     setCollections(
       featureCollections.data.map((collection) => {
@@ -42,16 +35,10 @@ const Toolbar = (props) => {
     );
   };
 
-  const onCollectionItemSelected = (collection) => {
-    setSelectedCollection(collection);
-  };
-
-  const onLoadHandler = async () => {
+  const onLoadHandler = async (selectedCollection) => {
     const collectionFeatures = await api.get(
       `/collections/${selectedCollection.id}/features`
     );
-    setSelectedCollection(null);
-    _onModalClose();
     const validGeoJson = {
       type: "FeatureCollection",
       features: collectionFeatures.data.map((feature) => {
@@ -64,45 +51,20 @@ const Toolbar = (props) => {
         };
       }),
     };
+    onSetActiveModalHandler(null);
     onLoad(validGeoJson);
   };
-  //#endregion
 
   return (
     <div className="toolbar-container">
       <ToolbarButton icon={faSave} onClick={onSaveHandler} />
       <ToolbarButton icon={faFolderOpen} onClick={onLoadClickHandler} />
       <Modal
-        isOpen={isModalOpen}
-        onRequestClose={_onModalClose}
+        isOpen={activeModal === "load-modal"}
+        onRequestClose={() => onSetActiveModalHandler(null)}
         style={{ overlay: { zIndex: 1001 }, content: { zIndex: 1001 } }} // compensate for leaflet z-index
       >
-        <header className="modal__header">
-          <h2>Load Feature Collection</h2>
-          <button
-            className="modal__select-button"
-            onClick={onLoadHandler}
-            disabled={selectedCollection === null}
-          >
-            Load Collection
-          </button>
-        </header>
-        <main>
-          {collections.map((collection) => {
-            return (
-              <div
-                className={`modal__collection-item ${
-                  collection === selectedCollection ? "selected" : ""
-                }`}
-                onClick={() => onCollectionItemSelected(collection)}
-                key={collection.id}
-              >
-                <span>{collection.name}</span>
-                <span>{collection.updatedAt.toLocaleString("default")}</span>
-              </div>
-            );
-          })}
-        </main>
+        <LoadModal collections={collections} onLoad={onLoadHandler} />
       </Modal>
     </div>
   );
